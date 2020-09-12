72e30fc780641fd0b821e60f61a4a676c2c6cb9c
Masayuki Onishi <omccello@gmail.com>
2020-09-12 15:32:48 +0900

    詳細画面を整理

--- lib/ui/pages/series_detail.dart
+++ lib/ui/pages/series_detail.dart
@@ -27,94 +27,88 @@ class _DetailsScreenState extends State<DetailsScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: FutureBuilder<Series>(
-          future: seriesDetail,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return _waitLoading();
-            }
+        body: _buildBody(),
+      ),
+    );
+  }
 
-            final data = snapshot.requireData;
-            return SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  GameTitleImage(
-                    imageUrl: data.assets['cover-large']['uri'] as String,
-                    title: data.nameInt,
-                  ),
-                  _spaceBox(height: 11),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    child: Column(
+  Widget _buildBody() {
+    return FutureBuilder<Series>(
+      future: seriesDetail,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return _waitLoading();
+        }
+
+        final data = snapshot.requireData;
+        return SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              GameTitleImage(
+                imageUrl: data.assets['cover-large']['uri'] as String,
+                title: data.nameInt,
+              ),
+              _spaceBox(height: 11),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      data.nameInt,
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                    _spaceBox(height: 7),
+                    _spaceBox(height: 13),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        Text(
-                          data.nameInt,
-                          style: Theme.of(context).textTheme.headline3,
-                        ),
-                        _spaceBox(height: 7),
-                        _spaceBox(height: 13),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        Column(
                           children: <Widget>[
-                            Column(
-                              children: <Widget>[
-                                Text(
-                                  'Year',
-                                  style: Theme.of(context).textTheme.caption,
-                                ),
-                                Text(
-                                  'null',
-                                  // '${DateTime?.parse(data.created)?.year}',
-                                  style: Theme.of(context).textTheme.subtitle1,
-                                ),
-                              ],
+                            Text(
+                              'Year: ',
+                              style: Theme.of(context).textTheme.caption,
                             ),
-                            Column(
-                              children: <Widget>[
-                                Text(
-                                  'abbreviation',
-                                  style: Theme.of(context).textTheme.caption,
-                                ),
-                                Text(
-                                  data.abbreviation,
-                                  style: Theme.of(context).textTheme.subtitle1,
-                                ),
-                              ],
-                            ),
-                            Column(
-                              children: <Widget>[
-                                Text(
-                                  'weblink',
-                                  style: Theme.of(context).textTheme.caption,
-                                ),
-                                Text(
-                                  data.webLink,
-                                  style: Theme.of(context).textTheme.subtitle1,
-                                ),
-                              ],
+                            Text(
+                              'null',
+                              // '${DateTime?.parse(data.created)?.year}',
+                              style: Theme.of(context).textTheme.subtitle1,
                             ),
                           ],
                         ),
-                        _spaceBox(height: 13),
-                        Text(
-                          data.nameInt,
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1
-                              .apply(fontSizeFactor: 1.2),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              'abbreviation',
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                            Text(
+                              data.abbreviation,
+                              style: Theme.of(context).textTheme.subtitle1,
+                            ),
+                          ],
                         ),
-                        _spaceBox(height: 13),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              'weblink',
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                            Text(
+                              data.webLink,
+                              style: Theme.of(context).textTheme.subtitle1,
+                            ),
+                          ],
+                        ),
                       ],
                     ),
-                  ),
-                  _spaceBox(height: 13),
-                ],
+                  ],
+                ),
               ),
-            );
-          },
-        ),
-      ),
+              _spaceBox(height: 13),
+            ],
+          ),
+        );
+      },
     );
   }
 
@@ -122,7 +116,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
     return const Center(child: CircularProgressIndicator());
   }
 
-  Widget _spaceBox({ double height = 15.0 }) {
+  Widget _spaceBox({double height = 15.0}) {
     return SizedBox(height: height);
   }
 }
